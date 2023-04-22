860833cdab4c5b6bdde40df6c7a0d6afaa1cb3ec
Shaxzodbek Bobobekov <shaxzodbekbobobekov@gmail.com>
2023-04-22 16:17:30 +0500

    chakra ui added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+import "../styles/globals.css";
+import "@fontsource/roboto";
+
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
